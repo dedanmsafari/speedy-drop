33fe31ca6dab7652dcbe365cd40c08dc65bd4c89
dedanmsafari <dedan.msafari@gmail.com>
2022-08-11 20:53:52 +0300

    themeing styles

---
+++ src/infrastructure/theme/fonts.js
@@ -0,0 +1,26 @@
+export const fonts = {
+  light: "Poppins_300Light",
+  body: "Poppins_400Regular",
+  heading: " Poppins_600SemiBold",
+  special: "OleoScriptSwashCaps_400Regular",
+  specialBold: " OleoScriptSwashCaps_700Bold",
+};
+
+export const fontWeights = {
+  light: 300,
+  regular: 400,
+  semiBold: 600,
+  bold: 700,
+};
+
+export const fontSizes = {
+  caption: "12px",
+  button: "14px",
+  body: "16px",
+  title: "20px",
+  h5: "24px",
+  h4: "34px",
+  h3: "45px",
+  h2: "56px",
+  h1: "112px",
+};
