ad93dab78135770956f4b47fe90bf781ad76c359
dedanmsafari <dedan.msafari@gmail.com>
2022-08-13 00:12:18 +0300

    app component

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./src/infrastructure/theme";
+import {
+  useFonts as usePoppins,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from "@expo-google-fonts/poppins";
+import {
+  useFonts as useOleoScriptSwashCaps,
+  OleoScriptSwashCaps_400Regular,
+  OleoScriptSwashCaps_700Bold,
+} from "@expo-google-fonts/oleo-script-swash-caps";
+import AppLoading from "expo-app-loading";
+
+import AppNavigator from "./src/infrastructure/navigation/app.navigator";
 
 export default function App() {
+  let [PoppinsLoaded] = usePoppins({
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  let [OleoScriptSwashCapsLoaded] = useOleoScriptSwashCaps({
+    OleoScriptSwashCaps_400Regular,
+    OleoScriptSwashCaps_700Bold,
+  });
+
+  if (!PoppinsLoaded || !OleoScriptSwashCapsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ThemeProvider theme={theme}>
+        <AppNavigator />
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
