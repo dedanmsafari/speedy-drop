063772ce27de95739b61896e21a4f0a3bfb38396
dedanmsafari <dedan.msafari@gmail.com>
2022-10-28 12:25:59 +0300

    updated app navigator

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,39 +1,29 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
-import HomeScreen from "../../features/Home/screens/home.screens";
-
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { View, Text } from "react-native";
 import { colors } from "../theme/colors";
-import { Feather } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 import HomeNavigator from "./home.navigator";
 
 const Tab = createMaterialBottomTabNavigator();
 
 const TAB_ICON = {
   Home: "home",
-  Records: "book",
-  Files: "file-text",
-  Profile: "user",
+  People: "md-people",
+  Profile: "person-sharp",
 };
 
 const createScreenOptions = ({ route }) => {
   const iconName = TAB_ICON[route.name];
   return {
-    tabBarIcon: ({ color, size = 20 }) => (
-      <Feather name={iconName} size={size} color={color} />
+    tabBarIcon: ({ color, size = 24 }) => (
+      <Ionicons name={iconName} size={size} color={color} />
     ),
   };
 };
 
-const FileScreen = () => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>File Screen</Text>
-    </View>
-  );
-};
 const RecordsScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
@@ -54,18 +44,17 @@ export default function AppNavigator() {
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={createScreenOptions}
-        shifting={true}
+        labeled={false}
+        activeColor={colors.brand.primary}
+        inactiveColor={colors.brand.muted}
         barStyle={{
-          backgroundColor: colors.brand.primary,
-          position: "absolute",
-          overflow: "hidden",
-          borderTopLeftRadius: 20,
-          borderTopRightRadius: 20,
+          backgroundColor: colors.bg.primary,
+          borderTopWidth: 1,
+          borderTopColor: colors.ui.tertiary,
         }}
       >
         <Tab.Screen name="Home" component={HomeNavigator} />
-        <Tab.Screen name="Records" component={RecordsScreen} />
-        <Tab.Screen name="Files" component={FileScreen} />
+        <Tab.Screen name="People" component={RecordsScreen} />
         <Tab.Screen name="Profile" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
