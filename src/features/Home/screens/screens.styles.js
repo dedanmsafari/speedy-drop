0fc4fa89cf54cb53bbde10ffaba3141b51fc46e4
dedanmsafari <dedan.msafari@gmail.com>
2022-11-01 03:42:00 +0300

    screen styles

--- src/features/Home/screens/screens.styles.js
+++ src/features/Home/screens/screens.styles.js
@@ -1,5 +1,23 @@
 import styled from "styled-components/native";
 
+export const TopBarContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 5px;
+  background-color: ${({ theme }) => theme.colors.bg.primary};
+`;
+export const IconContainer = styled.View`
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+export const LogoButtonHome = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  padding-left: 12px;
+`;
+
 export const TopBar = styled.View`
   flex-direction: row;
   align-items: center;
