f2d34a76293729e82093f7d724893030d6e7ad64
dedanmsafari <dedan.msafari@gmail.com>
2022-11-09 03:50:34 +0300

    homescreen details

--- src/features/Home/screens/home.screens.js
+++ src/features/Home/screens/home.screens.js
@@ -2,21 +2,18 @@ import { FlatList, TouchableOpacity, Image, View } from "react-native";
 import { IconButton } from "react-native-paper";
 import styled from "styled-components/native";
 import React from "react";
-import { Search } from "../../../components/search/search.component";
-import { Text } from "../../../components/text/text.component";
 
 import { Ionicons } from "@expo/vector-icons";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { AdditionButton } from "../../../components/search/search.styles";
 import MenuItemComponent from "../../../components/menu-item/menu-item.component";
-import { FlatFeed } from "expo-activity-feed";
 import { FadeInView } from "../../../animations/fade.animation";
 import {
   TopBarContainer,
   LogoButtonHome,
   IconContainer,
-  HomePage,
 } from "./screens.styles";
+
 import { colors } from "../../../infrastructure/theme/colors";
 
 const ProjectItems = Array(20).fill();
@@ -50,22 +47,22 @@ export default function HomeScreen({ navigation }) {
           />
         </IconContainer>
       </TopBarContainer>
-      <FlatFeed feedGroup="timeline" userId="dry-math-1" />
-      {/* <DataList
+
+      <DataList
         data={ProjectItems}
         renderItem={({ item }) => {
           return (
             <TouchableOpacity activeOpacity={0.4}>
               <FadeInView>
                 <Spacer position="bottom" size="xsmall">
-                  <FlatFeed />
+                  <MenuItemComponent />
                 </Spacer>
               </FadeInView>
             </TouchableOpacity>
           );
         }}
         key={(item) => item}
-      /> */}
+      />
       <AdditionButton>
         <IconButton icon="plus" size={25} color="white" />
       </AdditionButton>
