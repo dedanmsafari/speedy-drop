3cf761630ef03164becde59e038b046e7bd1b0c0
dedanmsafari <dedan.msafari@gmail.com>
2022-11-11 12:50:32 +0300

    changed the home screens

--- src/features/Home/screens/home.screens.js
+++ src/features/Home/screens/home.screens.js
@@ -1,71 +1,21 @@
-import { FlatList, TouchableOpacity, Image, View } from "react-native";
-import { IconButton } from "react-native-paper";
-import styled from "styled-components/native";
+import { FlatList, View } from "react-native";
+
 import React from "react";
 
-import { Ionicons } from "@expo/vector-icons";
-import { Spacer } from "../../../components/spacer/spacer.component";
-import { AdditionButton } from "../../../components/search/search.styles";
-import MenuItemComponent from "../../../components/menu-item/menu-item.component";
 import { FadeInView } from "../../../animations/fade.animation";
-import {
-  TopBarContainer,
-  LogoButtonHome,
-  IconContainer,
-} from "./screens.styles";
+
+import Post from "../components/post/post.component";
 
 import { colors } from "../../../infrastructure/theme/colors";
-
-const ProjectItems = Array(20).fill();
-
-const DataList = styled(FlatList).attrs({
-  contentContainerStyle: { padding: 1 },
-})``;
+import Interests from "../components/interests/interests.component";
+import TopBar from "../components/TopBar/topbar.component";
 
 export default function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, backgroundColor: colors.brand.secondary }}>
-      <TopBarContainer>
-        <LogoButtonHome activeOpacity={0.5}>
-          <Image
-            style={{ height: 35, width: 130 }}
-            source={require("../../../../assets/brandName.png")}
-          />
-          <Ionicons name="chevron-down-outline" size={20} color="black" />
-        </LogoButtonHome>
-        <IconContainer>
-          <IconButton icon="magnify" size={20} color={colors.brand.primary} />
-          <IconButton
-            icon="bell-outline"
-            size={20}
-            color={colors.brand.primary}
-          />
-          <IconButton
-            icon="message-reply-outline"
-            size={20}
-            color={colors.brand.primary}
-          />
-        </IconContainer>
-      </TopBarContainer>
-
-      <DataList
-        data={ProjectItems}
-        renderItem={({ item }) => {
-          return (
-            <TouchableOpacity activeOpacity={0.4}>
-              <FadeInView>
-                <Spacer position="bottom" size="xsmall">
-                  <MenuItemComponent />
-                </Spacer>
-              </FadeInView>
-            </TouchableOpacity>
-          );
-        }}
-        key={(item) => item}
-      />
-      <AdditionButton>
-        <IconButton icon="plus" size={25} color="white" />
-      </AdditionButton>
+      <TopBar />
+      <Post />
+      <Interests />
     </View>
   );
 }
