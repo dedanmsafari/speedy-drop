0b365285230f7f43e36779b4488f36165c8dd3b1
dedanmsafari <dedan.msafari@gmail.com>
2022-11-23 20:23:58 +0300

    home screen changes

--- src/features/Home/screens/home.screens.js
+++ src/features/Home/screens/home.screens.js
@@ -1,21 +1,50 @@
-import { FlatList, View } from "react-native";
+import { FlatList, View, ScrollView } from "react-native";
 
 import React from "react";
 
 import { FadeInView } from "../../../animations/fade.animation";
-
+import FeedData from "../components/feed/data";
 import Post from "../components/post/post.component";
-
+import styled from "styled-components/native";
 import { colors } from "../../../infrastructure/theme/colors";
+import { Spacer } from "../../../components/spacer/spacer.component";
 import Interests from "../components/interests/interests.component";
 import TopBar from "../components/TopBar/topbar.component";
+import Feed from "../components/feed/feed.component";
+
+// const FeedList = styled(FlatList).attrs({
+//   contentContainerStyle: { padding: 0 },
+// })``;
 
 export default function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, backgroundColor: colors.brand.secondary }}>
       <TopBar />
-      <Post />
-      <Interests />
+      <ScrollView>
+        <Post />
+        <Interests />
+        {FeedData.map((feedData) => (
+          <FadeInView>
+            <Spacer position="bottom" size="xsmall">
+              <Feed feed={feedData} />
+            </Spacer>
+          </FadeInView>
+        ))}
+
+        {/* <FeedList
+        data={FeedData}
+        renderItem={({ item }) => {
+          return (
+            <FadeInView>
+              <Spacer position="bottom" size="xsmall">
+                <Feed feed={item} />
+              </Spacer>
+            </FadeInView>
+          );
+        }}
+        keyExtractor={(item) => item.name}
+      /> */}
+      </ScrollView>
     </View>
   );
 }
