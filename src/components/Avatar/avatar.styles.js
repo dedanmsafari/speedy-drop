287914b7db3beb1aa2cff05cc646064e616912d4
dedanmsafari <dedan.msafari@gmail.com>
2022-11-23 20:25:11 +0300

    avatar styles

---
+++ src/components/Avatar/avatar.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const UserActive = styled.View`
+  width: 15px;
+  height: 15px;
+  border-radius: 8px;
+  background: #4bcb1f;
+  position: absolute;
+  bottom: -2px;
+  right: -2px;
+  border-width: 2px;
+  border-color: #ffffff;
+`;
