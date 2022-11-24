84fd587b1562bb8c52d9265e2560bed34837c23e
dedanmsafari <dedan.msafari@gmail.com>
2022-11-24 07:22:58 +0300

    feed styles

---
+++ src/features/Home/components/feed/feed.styles.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const FeedContainer = styled.View`
+  background-color: ${({ theme }) => theme.colors.bg.primary};
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+`;
+export const UserInfo = styled.View`
+  flex-direction: column;
+  padding: 0px 8px;
+`;
+
+export const LocationMarker = styled.View`
+  margin-left: auto;
+`;
+
+export const Body = styled.View``;
+export const TextContainer = styled.View`
+  padding: 5px 15px;
+`;
+export const Photo = styled.Image`
+  width: 100%;
+  height: 300px;
+`;
+
+export const Feedback = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 15px;
+`;
+export const Like = styled.View`
+  flex-direction: row;
+  width: 50px;
+  justify-content: space-between;
+`;
