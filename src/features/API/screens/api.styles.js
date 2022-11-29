fdf314274f2ba6481c426a252e7a145b12add3cd
dedanmsafari <dedan.msafari@gmail.com>
2022-11-29 22:11:15 +0300

    api styles

---
+++ src/features/API/screens/api.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+import { colors } from "../../../infrastructure/theme/colors";
+
+export const HomeContainer = styled.View`
+  flex: 1;
+  background-color: ${colors.bg.primary};
+  padding: 20px 10px 0px 10px; ;
+`;
+export const HeaderContainer = styled.View`
+  align-items: center;
+  text-align: center;
+`;
