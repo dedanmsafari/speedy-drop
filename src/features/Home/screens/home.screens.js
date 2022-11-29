1a29342e9de06b0f45a7fd2474bb4d5ba3af73e7
dedanmsafari <dedan.msafari@gmail.com>
2022-11-29 12:34:25 +0300

    home screen

--- src/features/Home/screens/home.screens.js
+++ src/features/Home/screens/home.screens.js
@@ -1,50 +1,143 @@
-import { View, ScrollView } from "react-native";
+import { Button } from "react-native";
 
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { TextInput } from "react-native-paper";
+import { useForm, Controller } from "react-hook-form";
 import { FadeInView } from "../../../animations/fade.animation";
-import FeedData from "../components/feed/data";
-import Post from "../components/post/post.component";
+import { HomeContainer, InputContainer } from "./home.styles";
 
 import { colors } from "../../../infrastructure/theme/colors";
 import { Spacer } from "../../../components/spacer/spacer.component";
-import Interests from "../components/interests/interests.component";
-import TopBar from "../components/TopBar/topbar.component";
-import Feed from "../components/feed/feed.component";
-
-// const FeedList = styled(FlatList).attrs({
-//   contentContainerStyle: { padding: 0 },
-// })``;
+import { Text } from "../../../components/text/text.component";
+import {
+  createCompanyDocumentFromInputs,
+  getCompanies,
+} from "../../../utils/firebase/firebase.utils";
+import { getDoc } from "firebase/firestore";
 
 export default function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, backgroundColor: colors.brand.secondary }}>
-      <TopBar />
-      <ScrollView>
-        <Post />
-        <Interests />
-        {FeedData.map((feedData) => (
-          <FadeInView key={feedData.userName}>
-            <Spacer position="bottom" size="xsmall">
-              <Feed feed={feedData} />
-            </Spacer>
-          </FadeInView>
-        ))}
+  const [snapShot, setSnapShot] = useState({});
+  const {
+    control,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      Name: "",
+      Industry: "",
+      Location: "",
+      Description: "",
+    },
+  });
 
-        {/* <FeedList
-        data={FeedData}
-        renderItem={({ item }) => {
-          return (
-            <FadeInView>
-              <Spacer position="bottom" size="xsmall">
-                <Feed feed={item} />
-              </Spacer>
-            </FadeInView>
-          );
+  const onSubmit = async (data) => {
+    const companyDocRef = await createCompanyDocumentFromInputs(data);
+    const companySnapShot = getDoc(companyDocRef);
+
+    setSnapShot(companySnapShot);
+    reset({
+      Name: "",
+      Industry: "",
+      Location: "",
+      Description: "",
+    });
+  };
+
+  useEffect(() => {
+    async function getData() {
+      const companyData = await getCompanies();
+    }
+
+    getData();
+  }, [snapShot]);
+
+  return (
+    <HomeContainer>
+      <Text variant="label">Enter your Company Details.</Text>
+      <Spacer position="bottom" size="medium" />
+
+      <Controller
+        control={control}
+        rules={{
+          required: true,
         }}
-        keyExtractor={(item) => item.name}
-      /> */}
-      </ScrollView>
-    </View>
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            label="Name"
+            mode="outlined"
+            onChangeText={onChange}
+            value={value}
+            activeOutlineColor={colors.text.success}
+          />
+        )}
+        name="Name"
+      />
+      {errors.Name && <Text variant="error">Company Name is required.</Text>}
+      <Spacer position="bottom" size="medium" />
+
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            label="Industry"
+            mode="outlined"
+            onChangeText={onChange}
+            value={value}
+            activeOutlineColor={colors.text.success}
+          />
+        )}
+        name="Industry"
+      />
+      {errors.Industry && <Text variant="error">Industry is required.</Text>}
+      <Spacer position="bottom" size="medium" />
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            label="location"
+            mode="outlined"
+            onChangeText={onChange}
+            value={value}
+            activeOutlineColor={colors.text.success}
+          />
+        )}
+        name="Location"
+      />
+      {errors.Location && <Text variant="error">Location is required.</Text>}
+      <Spacer position="bottom" size="medium" />
+
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+          minLength: 10,
+        }}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            label="Description"
+            mode="outlined"
+            multiline={true}
+            numberOfLines={5}
+            onChangeText={onChange}
+            value={value}
+            activeOutlineColor={colors.text.success}
+          />
+        )}
+        name="Description"
+      />
+      {errors.Description && (
+        <Text variant="error">Company Description is too short.</Text>
+      )}
+      <Spacer position="bottom" size="large" />
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+    </HomeContainer>
   );
 }
