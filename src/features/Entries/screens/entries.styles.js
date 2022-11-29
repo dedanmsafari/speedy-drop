1fdfe21a15812e16f1c9dcc60955c5ef96b8bb17
dedanmsafari <dedan.msafari@gmail.com>
2022-11-29 12:34:17 +0300

    entries stylers

---
+++ src/features/Entries/screens/entries.styles.js
@@ -0,0 +1,39 @@
+import styled from "styled-components/native";
+import { colors } from "../../../infrastructure/theme/colors";
+
+export const HomeContainer = styled.View`
+  flex: 1;
+  background-color: ${colors.bg.primary};
+  padding: 20px 10px 0px 10px; ;
+`;
+
+export const SortContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+export const ListSort = styled.TouchableOpacity`
+  flex-direction: row;
+`;
+export const CardSort = styled.TouchableOpacity`
+  flex-direction: row;
+`;
+
+export const CardContainer = styled.View`
+  background-color: ${colors.bg.secondary};
+  padding: 5px 8px;
+  border-radius: 10px;
+  elevation: 5px;
+`;
+export const CompanyName = styled.View`
+  align-items: center;
+`;
+export const Industry = styled.View``;
+export const Description = styled.View`
+  align-items: center;
+`;
+
+export const BottomDetails = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
