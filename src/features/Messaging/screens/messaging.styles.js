0a1fb71c7425bd8c9490d58c88ec296cdf12273c
dedanmsafari <dedan.msafari@gmail.com>
2022-11-29 22:11:58 +0300

    messaging styles

---
+++ src/features/Messaging/screens/messaging.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+import { colors } from "../../../infrastructure/theme/colors";
+
+export const HomeContainer = styled.View`
+  flex: 1;
+  background-color: ${colors.bg.primary};
+  padding: 20px 10px 0px 10px;
+  align-items: center;
+`;
