c8a200d6512325dd70169e711336477571ae9c6c
dedanmsafari <dedan.msafari@gmail.com>
2022-11-29 12:34:34 +0300

    home styles

---
+++ src/features/Home/screens/home.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { colors } from "../../../infrastructure/theme/colors";
+
+export const HomeContainer = styled.View`
+  flex: 1;
+  background-color: ${colors.bg.primary};
+  padding: 50px 20px;
+`;
