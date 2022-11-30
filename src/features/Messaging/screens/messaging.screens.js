d7f194f598b868839f7693a0fc3b5edb14572c32
dedanmsafari <dedan.msafari@gmail.com>
2022-11-30 07:18:07 +0300

    undo minor changes causing crashes

--- src/features/Messaging/screens/messaging.screens.js
+++ src/features/Messaging/screens/messaging.screens.js
@@ -5,61 +5,61 @@ import { StyledSafeAreaView } from "../../../utils/safeArea.util.component";
 import { Text } from "../../../components/text/text.component";
 import { HomeContainer } from "./messaging.styles";
 
-import messaging from "@react-native-firebase/messaging";
+// import messaging from "@react-native-firebase/messaging";
 
-async function requestUserPermission() {
-  const authStatus = await messaging().requestPermission();
-  const enabled =
-    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+// async function requestUserPermission() {
+//   const authStatus = await messaging().requestPermission();
+//   const enabled =
+//     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+//     authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-  if (enabled) {
-    console.log("Authorization status:", authStatus);
-  }
-}
+//   if (enabled) {
+//     console.log("Authorization status:", authStatus);
+//   }
+// }
 
-useEffect(() => {
-  if (requestUserPermission()) {
-    //return fcm token for the device
+// useEffect(() => {
+//   if (requestUserPermission()) {
+//     //return fcm token for the device
 
-    messaging()
-      .getToken()
-      .then((token) => {
-        console.log(token);
-      });
-  } else {
-    console.log("Failed token status", authStatus);
-  }
+//     messaging()
+//       .getToken()
+//       .then((token) => {
+//         console.log(token);
+//       });
+//   } else {
+//     console.log("Failed token status", authStatus);
+//   }
 
-  messaging()
-    .getInitialNotification()
-    .then(async (remoteMessage) => {
-      if (remoteMessage) {
-        console.log(
-          "Notification caused app to open from quit state:",
-          remoteMessage.notification
-        );
-      }
-    });
+//   messaging()
+//     .getInitialNotification()
+//     .then(async (remoteMessage) => {
+//       if (remoteMessage) {
+//         console.log(
+//           "Notification caused app to open from quit state:",
+//           remoteMessage.notification
+//         );
+//       }
+//     });
 
-  messaging().onNotificationOpenedApp(async (remoteMessage) => {
-    console.log(
-      "Notification caused app to open from background state:",
-      remoteMessage.notification
-    );
-  });
+//   messaging().onNotificationOpenedApp(async (remoteMessage) => {
+//     console.log(
+//       "Notification caused app to open from background state:",
+//       remoteMessage.notification
+//     );
+//   });
 
-  // Register background handler
-  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-    console.log("Message handled in the background!", remoteMessage);
-  });
+//   // Register background handler
+//   messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+//     console.log("Message handled in the background!", remoteMessage);
+//   });
 
-  const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-    Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
-  });
+//   const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+//     Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+//   });
 
-  return unsubscribe;
-}, []);
+//   return unsubscribe;
+// }, []);
 
 const MessagingScreen = () => {
   return (
